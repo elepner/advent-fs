aff25f255097944632e1177b8e80a0eca77851a4
Eduard Lepner <eduard@invarion.com>
2023-12-04 00:07:57 +0100

    wip

--- Day1/Day1.fs
+++ Day1/Day1.fs
@@ -2,17 +2,37 @@
 namespace AdventFS
 
 module Day1 =
-    let parseString str: string = 
+
+    let parseString2 (str: string) = 
         let replacements: (string * string) list= [
             ("zero", "0");
-            ("one", "1");
+            ("eight", "8");
             ("two", "2");
+            ("one", "1");
             ("three", "3");
             ("four", "4");
             ("five", "5");
             ("six", "6");
             ("seven", "7");
+            ("nine", "9")
+            ]
+        replacements
+        |> Seq.map(fun (word, num) -> (str.IndexOf(word), num))
+        |> Seq.filter(fun (idx, _) -> idx >= 0)
+        |> Seq.minBy(fun (idx, _) -> idx)
+        
+
+    let parseString str: string = 
+        let replacements: (string * string) list= [
+            ("zero", "0");
             ("eight", "8");
+            ("two", "2");
+            ("one", "1");
+            ("three", "3");
+            ("four", "4");
+            ("five", "5");
+            ("six", "6");
+            ("seven", "7");
             ("nine", "9")
             ]
         replacements
@@ -21,6 +41,9 @@ module Day1 =
     let solve filePath =
         System.IO.File.ReadLines(filePath)
         |> Seq.map(parseString)
+        |> Seq.map(fun line -> 
+            printfn "%s" line
+            line)
         |> Seq.map(fun line -> Seq.filter System.Char.IsDigit line)
         |> Seq.map(fun digits -> 
             let first = Seq.tryHead digits
@@ -31,6 +54,9 @@ module Day1 =
             | (Some f, Some l) -> Some (f, l)
             | _ -> None)
         |> Seq.map(fun (f, l) -> string f + string l)
+        |> Seq.map(fun line -> 
+            printfn "%s" line
+            line)
         |> Seq.map(fun num -> int num)
         |> Seq.sum
 
